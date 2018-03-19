5ca73c3212cb6d683c6edfe29395d7c11c64b048
mahongquan <mahongquan@sina.com>
2018-03-19 13:35:36 +0800

    all inject

--- content_scripts/content_script.js
+++ content_scripts/content_script.js
@@ -1,4 +1,4 @@
-console.log("hi");
+console.log(window.location.href);
 const scriptLoader = ({src, innerHTML}) => {
     if (src) {
         return new Promise((resolve, reject) => {
@@ -18,8 +18,9 @@ const scriptLoader = ({src, innerHTML}) => {
     document.body.appendChild(theScript);
     return theScript;
 }
-
-co(function*() {
+//console.log();
+if(window.location.href.indexOf("http://oa.ncschina.com/seeyon/main.do")==0){
+  co(function*() {
 // 加载脚本
 //yield scriptLoader({ src: '//cdn.bootcss.com/jquery/3.2.1/jquery.js' });
 //yield scriptLoader({ src: '//cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.js' });
@@ -102,6 +103,9 @@ function showmenu(i, j) {
   var url = '/seeyon' + menuitem.url; //'/seeyon/portal/spaceController.do?method=showThemSpace&themType=19'
   showMenu(url, menuitem.id, 2, menuitem.target, menuitem.name, menuitem.resourceCode, '-4751259066364441679')
 }
+function xinjian() {
+  showmenu(0, 1);
+}
 
 function daiban() {
   showmenu(0, 4);
@@ -166,7 +170,7 @@ console.log(beastImage);
 if (!beastImage) {
   beastImage = document.createElement("div");
   beastImage.setAttribute("id", "madiv1");
-  beastImage.setAttribute("style", "background:#66FF66"); // style="font-size:14px;width:600px;background-color:#FF0066
+  beastImage.setAttribute("style", "background:#aaaaaa"); // style="font-size:14px;width:600px;background-color:#FF0066
   var existingItem = document.body.firstElementChild;
   document.body.insertBefore(beastImage, existingItem);
 } else {
@@ -188,6 +192,10 @@ beastImage.appendChild(newe);
 newe.insertAdjacentText("afterBegin", "个人");
 newe.onclick = personSpace;
 
+var newe = document.createElement("button");
+beastImage.appendChild(newe);
+newe.insertAdjacentText("afterBegin", "新建");
+newe.onclick = xinjian;
 
 var newe = document.createElement("button");
 beastImage.appendChild(newe);
@@ -202,6 +210,8 @@ newe.onclick = yiban;
 var newe = document.createElement("input");
 beastImage.appendChild(newe);
 newe.setAttribute("id", "mainput1");
+newe.setAttribute("value", "erp");
+
 newe.onclick = yiban;
 var newe = document.createElement("button");
 beastImage.appendChild(newe);
@@ -213,5 +223,138 @@ newe.onclick = chazhao;
 //console.assert(window.jQuery.cookie, 'jQuery.cookie is not defined');
 //console.log(window.jQuery.cookie);
     `,
-});
-});
+});//loader 
+});//co
+}
+//method=listPending&_resourceCode=F01_listPending
+else if(window.location.href.indexOf("http://oa.ncschina.com/seeyon/collaboration/collaboration.do?method=listPending")==0){
+  co(function*() {
+// 加载脚本
+//yield scriptLoader({ src: '//cdn.bootcss.com/jquery/3.2.1/jquery.js' });
+//yield scriptLoader({ src: '//cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.js' });
+
+// 检查jquery和jquery.cookie是否顺利注入。注意检查代码也要注入到页面环境
+scriptLoader({
+    innerHTML: `
+console.log("inject================")
+function chazhao() {
+  console.log("chazhao========");
+  var tofind = $("#mainput1").val();
+  var iframes=$(document).find("select");
+  $(iframes[0]).css("display","inline");
+  $(iframes[0]).val("subject").trigger("change")
+  //$(iframes[0]).css("display","none");
+  //subject_container
+  var iframes=$(document).find("#subject_container");
+  iframes.removeClass("hidden")
+  var iframes=$(document).find(".search_input");
+  iframes.val(tofind);
+
+  var iframes=$(document).find(".search_btn");
+  iframes.trigger("click");
+}
+var beastImage = document.getElementById("madiv1");
+console.log(beastImage);
+if (!beastImage) {
+  beastImage = document.createElement("div");
+  beastImage.setAttribute("id", "madiv1");
+  beastImage.setAttribute("style", "background:#AAAAAA"); // style="font-size:14px;width:600px;background-color:#FF0066
+  var existingItem = document.body.firstElementChild;
+  document.body.insertBefore(beastImage, existingItem);
+} else {
+  beastImage.innerHTML = "";
+}
+var newe = document.createElement("input");
+beastImage.appendChild(newe);
+newe.setAttribute("id", "mainput1");
+newe.setAttribute("value", "erp");
+
+
+var newe = document.createElement("button");
+beastImage.appendChild(newe);
+newe.insertAdjacentText("afterBegin", "查找");
+newe.onclick = chazhao;
+var newe = document.createTextNode(window.location.href);
+beastImage.appendChild(newe);
+    `,
+});//loader
+});//co
+
+}//else
+//http://oa.ncschina.com/seeyon/collaboration/collaboration.do?method=newColl
+else if(window.location.href.indexOf("http://oa.ncschina.com/seeyon/collaboration/collaboration.do?method=newColl")==0){
+  co(function*() {
+// 加载脚本
+//yield scriptLoader({ src: '//cdn.bootcss.com/jquery/3.2.1/jquery.js' });
+//yield scriptLoader({ src: '//cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.js' });
+
+// 检查jquery和jquery.cookie是否顺利注入。注意检查代码也要注入到页面环境
+scriptLoader({
+    innerHTML: `
+console.log("inject================")
+function setbiaoti() {
+  console.log("chazhao========");
+  var tofind = $("#mainput1").val();
+  
+  var iframes=$(document).find("#subject");
+  iframes.val(tofind);
+
+}
+var beastImage = document.getElementById("madiv1");
+console.log(beastImage);
+if (!beastImage) {
+  beastImage = document.createElement("div");
+  beastImage.setAttribute("id", "madiv1");
+  beastImage.setAttribute("style", "background:#AAAAAA"); // style="font-size:14px;width:600px;background-color:#FF0066
+  var existingItem = document.body.firstElementChild;
+  document.body.insertBefore(beastImage, existingItem);
+} else {
+  beastImage.innerHTML = "";
+}
+var newe = document.createElement("input");
+beastImage.appendChild(newe);
+newe.setAttribute("id", "mainput1");
+newe.setAttribute("value", "erp");
+
+
+var newe = document.createElement("button");
+beastImage.appendChild(newe);
+newe.insertAdjacentText("afterBegin", "标题");
+newe.onclick = setbiaoti;
+
+var newe = document.createTextNode(window.location.href);
+beastImage.appendChild(newe);
+    `,
+});//loader
+});//co
+
+}//else
+else{
+  co(function*() {
+   console.log("else load");
+// 加载脚本
+//yield scriptLoader({ src: '//cdn.bootcss.com/jquery/3.2.1/jquery.js' });
+//yield scriptLoader({ src: '//cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.js' });
+
+// 检查jquery和jquery.cookie是否顺利注入。注意检查代码也要注入到页面环境
+scriptLoader({
+    innerHTML: `
+console.log("inject================")
+var beastImage = document.getElementById("madiv1");
+console.log(beastImage);
+if (!beastImage) {
+  beastImage = document.createElement("div");
+  beastImage.setAttribute("id", "madiv1");
+  beastImage.setAttribute("style", "background:#AAAAAA"); // style="font-size:14px;width:600px;background-color:#FF0066
+  var existingItem = document.body.firstElementChild;
+  document.body.insertBefore(beastImage, existingItem);
+} else {
+  beastImage.innerHTML = "";
+}
+var newe = document.createTextNode(window.location.href);
+beastImage.appendChild(newe);
+    `,
+});//loader
+});//co
+
+}//else
